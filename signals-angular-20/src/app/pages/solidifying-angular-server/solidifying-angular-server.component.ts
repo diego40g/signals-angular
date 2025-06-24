593af1f40b0427114467fc0123a0691e1184ed6a
diego40g <diegopaznaula@hotmail.com>
2025-06-23 22:37:32 -0500

    feat: add SolidifyingAngularServer component with template, styles, and tests

---
+++ signals-angular-20/src/app/pages/solidifying-angular-server/solidifying-angular-server.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-solidifying-angular-server',
+  imports: [],
+  templateUrl: './solidifying-angular-server.component.html',
+  styleUrl: './solidifying-angular-server.component.sass'
+})
+export class SolidifyingAngularServerComponent {
+
+}
