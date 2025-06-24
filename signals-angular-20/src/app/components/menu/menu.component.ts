8381db71adbff49b16e9fe995a0318558124030e
diego40g <diegopaznaula@hotmail.com>
2025-06-23 22:38:52 -0500

    feat: add MenuComponent with HTML, SASS, and unit tests

---
+++ signals-angular-20/src/app/components/menu/menu.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-menu',
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.sass'
+})
+export class MenuComponent {
+  menuItems = [
+    { path: '/new-experimental-apis', title: 'New experimental APIs' },
+    { path: '/promoting-zoneless', title: 'Promoting Zoneless' },
+    { path: '/solidifying-angular-server', title: 'Solidifying Angular on the server' },
+    { path: '/polishing-developer-experience', title: 'Polishing developer experience' },
+    { path: '/deprecation-ngif-ngfor-ngswitch', title: 'Deprecation of NgIf, NgFor...' }
+  ];
+}
