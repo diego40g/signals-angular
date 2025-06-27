cff5f30657174f0ebe6274b130542c9986be77b2
diego40g <diegopaznaula@hotmail.com>
2025-06-26 22:51:25 -0500

    feat: agregar componente DataStream con su plantilla y estilos

---
+++ signals-angular-20/src/app/pages/new-experimental-apis/components/data-stream/data-stream.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-stream',
+  imports: [],
+  templateUrl: './data-stream.component.html',
+  styleUrl: './data-stream.component.sass'
+})
+export class DataStreamComponent {
+
+}
