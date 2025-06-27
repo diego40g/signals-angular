d102be345ddb1c35d572e2cb7cc3c3accd9d3e0a
diego40g <diegopaznaula@hotmail.com>
2025-06-26 22:51:29 -0500

    feat: agregar componente HttpResource con su plantilla, estilos y pruebas

---
+++ signals-angular-20/src/app/pages/new-experimental-apis/components/http-resource/http-resource.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-http-resource',
+  imports: [],
+  templateUrl: './http-resource.component.html',
+  styleUrl: './http-resource.component.sass'
+})
+export class HttpResourceComponent {
+
+}
