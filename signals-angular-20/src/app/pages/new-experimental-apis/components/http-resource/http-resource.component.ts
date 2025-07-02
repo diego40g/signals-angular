da8a9f881387388ef50fa1bc898fae9c1be71fa0
diego40g <diegopaznaula@hotmail.com>
2025-07-01 22:03:02 -0500

    feat: implementar componente HttpResource con gestión de usuarios y estilos

--- signals-angular-20/src/app/pages/new-experimental-apis/components/http-resource/http-resource.component.ts
+++ signals-angular-20/src/app/pages/new-experimental-apis/components/http-resource/http-resource.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { httpResource } from '@angular/common/http';
+import { Component, signal } from '@angular/core';
+import { User } from '@app/models/user';
 
 @Component({
   selector: 'app-http-resource',
   standalone: true,
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './http-resource.component.html',
   styleUrl: './http-resource.component.sass'
 })
 export class HttpResourceComponent {
+  userId = signal(1);
 
+  userResource = httpResource<User>(() =>
+    `https://jsonplaceholder.typicode.com/users/${this.userId()}`,
+  );
+
+  changeUserId(id: number) {
+    this.userId.set(id);
+  }
 }
