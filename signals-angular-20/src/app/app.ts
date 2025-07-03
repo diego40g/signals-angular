be66074c6124dae2e860c17d5573af38a26cc767
diego40g <diegopaznaula@hotmail.com>
2025-07-02 19:09:41 -0500

    fix: corregir la ruta de importación del componente MenuComponent

--- signals-angular-20/src/app/app.ts
+++ signals-angular-20/src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { MenuComponent } from './components/menu/menu.component';
+import { MenuComponent } from '@components/menu/menu.component';
 
 @Component({
   selector: 'app-root',
