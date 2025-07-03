149440a8c20465b8e18092245435507318b420c1
diego40g <diegopaznaula@hotmail.com>
2025-07-02 22:08:22 -0500

    test: update AppController test to check for correct status response

--- bknd-nest-11/src/app.controller.spec.ts
+++ bknd-nest-11/src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return the correct status', () => {
+      expect(appController.getStatus()).toBe('Status OK');
     });
   });
 });
