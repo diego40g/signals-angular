28bc1621aae9d92a3d776d2dd7b9a4dcbb259d45
Diego Ignacio Paz Naula <diegopaznaula@hotmail.com>
2025-08-02 11:14:51 -0500

    Api rest/new experimental/dp/f002 (#3)
    * refactor: replace <ng-content> with <ng-container> in multiple components
    
    * fix: correct document creation for Swagger setup in main.ts
    
    * test: update AppController test to check for correct status response

--- bknd-nest-11/src/main.ts
+++ bknd-nest-11/src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   .addBearerAuth()
   .build();
   
-  const document = () => SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
