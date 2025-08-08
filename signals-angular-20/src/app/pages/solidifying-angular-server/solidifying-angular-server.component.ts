7de7555a958b7e7009efb08c66d46f6b901fa869
diego40g <diegopaznaula@hotmail.com>
2025-08-07 19:21:34 -0500

    feat: implementar componente SolidifyingAngularServer con navegación y estilos

--- signals-angular-20/src/app/pages/solidifying-angular-server/solidifying-angular-server.component.ts
+++ signals-angular-20/src/app/pages/solidifying-angular-server/solidifying-angular-server.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { InformationComponent } from './components/information/information.component';
+import { HydrateOnViewportComponent } from './components/hydrate-on-viewport/hydrate-on-viewport.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-solidifying-angular-server',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, InformationComponent, HydrateOnViewportComponent],
   templateUrl: './solidifying-angular-server.component.html',
   styleUrl: './solidifying-angular-server.component.sass'
 })
 export class SolidifyingAngularServerComponent {
+  activeTab: string = 'information';
 
+menuItems = [
+    { id: 'information', label: 'Informacion de Hidratación incremental y rutas', icon: '💧' },
+    { id: 'hydrate-on-viewport', label: 'Demo: Hidratación incremental', icon: '⚡' }
+  ];
+
+  setActiveTab(tabId: string): void {
+    this.activeTab = tabId;
+  }
+
+  isActiveTab(tabId: string): boolean {
+    return this.activeTab === tabId;
+  }
 }
