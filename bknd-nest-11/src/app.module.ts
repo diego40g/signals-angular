a8fd5df02d04b1cdf50115e7f616ffa2ca27ebda
diego40g <diegopaznaula@hotmail.com>
2025-08-19 22:26:29 -0500

    feat: agregar PrismaModule y servicios de Products y Users al módulo HydrateRest

--- bknd-nest-11/src/app.module.ts
+++ bknd-nest-11/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WebSocketExampleModule } from './web-socket-example/web-socket-example.module';
 import { HydrateRestModule } from './hydrate-rest/hydrate-rest.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [WebSocketExampleModule, HydrateRestModule],
+  imports: [WebSocketExampleModule, HydrateRestModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- bknd-nest-11/src/hydrate-rest/hydrate-rest.module.ts
+++ bknd-nest-11/src/hydrate-rest/hydrate-rest.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { HydrateRestController } from './hydrate-rest.controller';
+import { ProductsService } from './services/products/products.service';
+import { UsersService } from './services/users/users.service';
+import { ProductsController } from './controller/products/products.controller';
+import { UsersController } from './controller/users/users.controller';
 
 @Module({
-  controllers: [HydrateRestController]
+  controllers: [HydrateRestController, ProductsController, UsersController],
+  providers: [ProductsService, UsersService]
 })
 export class HydrateRestModule {}
