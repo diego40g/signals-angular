b04fa981fc3b6db91a70eecb8cb12ce232f8f66d
diego40g <diegopaznaula@hotmail.com>
2025-08-19 22:24:34 -0500

    feat: agregar migraciones para las tablas Product y User en Prisma

--- bknd-nest-11/prisma/migrations/20250811034326_init/migration.sql
+++ bknd-nest-11/prisma/migrations/20250811034326_init/migration.sql
@@ -6,8 +6,20 @@ CREATE TABLE "Product" (
     "price" REAL NOT NULL,
     "category" TEXT NOT NULL,
     "description" TEXT NOT NULL,
-    "imageBase64" TEXT NOT NULL
+    "image" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "refreshToken" TEXT
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Product_slug_key" ON "Product"("slug");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
