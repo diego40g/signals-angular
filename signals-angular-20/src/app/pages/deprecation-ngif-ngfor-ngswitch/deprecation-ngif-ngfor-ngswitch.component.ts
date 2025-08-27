6838ca169bb6927e273923dd4bb1e6b6d0771cb4
diego40g <diegopaznaula@hotmail.com>
2025-08-26 20:12:31 -0500

    feat: Implementar navegación y contenido para la sección de desuso de NgIf, NgFor y NgSwitch

--- signals-angular-20/src/app/pages/deprecation-ngif-ngfor-ngswitch/deprecation-ngif-ngfor-ngswitch.component.ts
+++ signals-angular-20/src/app/pages/deprecation-ngif-ngfor-ngswitch/deprecation-ngif-ngfor-ngswitch.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { User } from './models/user';
+import { CommonModule } from '@angular/common';
+import { NgifDemoComponent } from './components/ngif-demo/ngif-demo.component';
+import { NgforDemoComponent } from './components/ngfor-demo/ngfor-demo.component';
+import { NgswitchDemoComponent } from './components/ngswitch-demo/ngswitch-demo.component';
 
 @Component({
   selector: 'app-deprecation-ngif-ngfor-ngswitch',
-  imports: [],
+  imports: [CommonModule, NgifDemoComponent, NgforDemoComponent, NgswitchDemoComponent],
   templateUrl: './deprecation-ngif-ngfor-ngswitch.component.html',
   styleUrl: './deprecation-ngif-ngfor-ngswitch.component.sass'
 })
 export class DeprecationNgifNgforNgswitchComponent {
+  selectedTab = 'ngif';
 
+  changeTab(tab: string): void {
+    this.selectedTab = tab;
+  }
 }
